f82adf33a220fcc1f2c768e21dbe2d5951b60fba
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2014-10-10 12:01:42 +0700

    New email registration

--- bcbk5/registration/__init__.py
+++ bcbk5/registration/__init__.py
@@ -1,19 +1,21 @@
 from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
+from django.contrib.staticfiles import finders
 
 from registration.models import Registration
 
 @receiver(post_save)
 def db_save(sender, instance, created, **kwargs):
 	if sender == Registration and created and instance.email:
-		send_mail(
-			'Barcamp Bangkhen 5 Registration',
+		email = EmailMultiAlternatives(
+			'Barcamp Bangkhen 2014 Registration Confirmation',
 			render_to_string('registration/email_regis.txt', {'regis': instance}),
 			settings.DEFAULT_FROM_EMAIL,
-			[instance.email],
-			True,
-			html_message=render_to_string('registration/email_regis.html', {'regis': instance})
+			['{0} <{1}>'.format(instance.name, instance.email)],
 		)
+		email.attach_file(finders.find_location('barcamp.ics'))
+		email.attach_alternative(render_to_string('registration/email_regis.html', {'regis': instance}), 'text/html')
+		email.send(True)
